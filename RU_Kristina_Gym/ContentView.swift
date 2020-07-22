7a7c522ae27921aca82c6105258d51557f5f33cd
Kristina <perekrist31@gmail.com>
2020-07-22 14:25:43 +0700

    Initial Commit

---
+++ RU_Kristina_Gym/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RU_Kristina_Gym
+//
+//  Created by Кристина Перегудова on 22.07.2020.
+//  Copyright © 2020 Кристина Перегудова. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
