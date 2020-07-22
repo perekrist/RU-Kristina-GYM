883fcdc0842b991a67b3f15094740e1435810f32
Kristina <perekrist31@gmail.com>
2020-07-22 15:03:17 +0700

    step 1/5

--- RU_Kristina_Gym/ContentView.swift
+++ RU_Kristina_Gym/ContentView.swift
@@ -10,7 +10,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Color(#colorLiteral(red: 0.4350972176, green: 0.6101457477, blue: 0.8216096163, alpha: 1)).edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Step 1/5")
+                    .font(.system(size: 50))
+                    .foregroundColor(.white)
+                
+                Text("What is your purpose?")
+                    .font(.system(size: 18))
+                    .foregroundColor(.white)
+                    .padding(.vertical, 50)
+                
+                VStack {
+                    Button(action: {
+                        
+                    }) {
+                        ZStack {
+                            HStack {
+                                Image("weight")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                    .padding()
+                                Spacer()
+                            }
+                            HStack {
+                                
+                                
+                                Spacer()
+                                Text("Weight loss")
+                                    .font(.system(size: 24))
+                                    .padding()
+                                Spacer()
+                            }
+                        }
+                    }
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .padding()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        ZStack {
+                            HStack {
+                                Image("yoga")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                    .padding()
+                                Spacer()
+                            }
+                            HStack {
+                                
+                                
+                                Spacer()
+                                Text("Keeping fit")
+                                    .font(.system(size: 24))
+                                    .padding()
+                                Spacer()
+                            }
+                        }
+                    }
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .padding()
+                    
+                    Button(action: {
+                        
+                    }) {
+                        ZStack {
+                            HStack {
+                                Image("triceps")
+                                    .resizable()
+                                    .frame(width: 35, height: 35)
+                                    .padding()
+                                Spacer()
+                            }
+                            HStack {
+                                
+                                
+                                Spacer()
+                                Text("Build muscle")
+                                    .font(.system(size: 24))
+                                    .padding()
+                                Spacer()
+                            }
+                        }
+                    }
+                    .background(Color.white)
+                    .cornerRadius(30)
+                    .padding()
+                }
+            }
+        }
     }
 }
 
