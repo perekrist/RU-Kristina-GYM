76c322907eee9ef8ac58c556a88e571d51e640bf
Kristina <perekrist31@gmail.com>
2020-07-23 16:17:31 +0700

    profile

--- RU_Kristina_Gym/View/Profile.swift
+++ RU_Kristina_Gym/View/Profile.swift
@@ -14,24 +14,160 @@ struct Profile: View {
     
     @State var sigIn = false
     
-    var body: some View {
-        Button(action: {
-            self.viewModel.signout(username: UserDefaults.standard.value(forKey: "username") as? String ?? "", completion: {
-                self.sigIn.toggle()
-                self.removeDataFromMemory()
-            })
-        }) {
-            Text("Sign out")
-            .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
-                .font(.system(size: 50))
-        }.navigate(to: SignIn(), when: $sigIn)
-    }
+    @State var gender = "male"
+    @State var weight = "80"
+    @State var height = "175"
+    @State var username = "Ivanov Ivan"
     
-    func removeDataFromMemory() {
-//        UserDefaults.standard.removeObject(forKey: "gender")
-//        UserDefaults.standard.removeObject(forKey: "weight")
-//        UserDefaults.standard.removeObject(forKey: "height")
-//        UserDefaults.standard.removeObject(forKey: "isNotFirstTime")
+    @State var toggle = true
+    
+    var body: some View {
+        VStack {
+            ZStack {
+                LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 0.3866932392, green: 0.6149073243, blue: 0.8426337838, alpha: 1)), Color(#colorLiteral(red: 0.7710116506, green: 0.8286624551, blue: 0.8925099969, alpha: 1))]), startPoint: .leading, endPoint: .trailing)
+                    .edgesIgnoringSafeArea(.top)
+                    .frame(height: 98)
+                
+                HStack {
+                    VStack {
+                        Text(weight)
+                            .font(.system(size: 14))
+                            .foregroundColor(.white)
+                        
+                        Text("Weight")
+                            .font(.system(size: 14))
+                            .foregroundColor(.white)
+                    }.padding()
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text(username)
+                            .font(.system(size: 24))
+                            .foregroundColor(.white)
+                        
+                        Text(gender)
+                            .font(.system(size: 16))
+                            .foregroundColor(.white)
+                    }.padding()
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text(height)
+                            .font(.system(size: 14))
+                            .foregroundColor(.white)
+                        
+                        Text("Height")
+                            .font(.system(size: 14))
+                            .foregroundColor(.white)
+                    }.padding()
+                }
+                
+            }
+            
+            VStack {
+                HStack {
+                    Text("Training rest")
+                        .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                        .font(.system(size: 17))
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                    
+                    Spacer()
+                    
+                    Text("30 sec")
+                        .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                        .font(.system(size: 17))
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                }
+                Divider()
+                HStack {
+                    Text("Notifications")
+                        .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                        .font(.system(size: 17))
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                    
+                    Spacer()
+                    
+                }
+                Divider()
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Biometric")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                            .font(.system(size: 17))
+                            .padding(.horizontal)
+                            .padding(.vertical, 10)
+                        
+                        Spacer()
+                    }
+                    
+                }
+                Divider()
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Start dialog")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                            .font(.system(size: 17))
+                            .padding(.horizontal)
+                            .padding(.vertical, 10)
+                        
+                        Spacer()
+                    }
+                }
+                Divider()
+                HStack {
+                    Button(action: {
+                        
+                    }) {
+                        Text("Privacy pilicy")
+                            .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                            .font(.system(size: 17))
+                            .padding(.horizontal)
+                            .padding(.vertical, 10)
+                        
+                        Spacer()
+                    }
+                }
+                Divider()
+            }
+            
+            Button(action: {
+                self.viewModel.signout(username: UserDefaults.standard.value(forKey: "username") as? String ?? "", completion: {
+                    self.sigIn.toggle()
+                })
+            }) {
+                HStack {
+                    Spacer()
+                    Text("Sign out")
+                        .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                        .font(.system(size: 24))
+                        .padding(.horizontal)
+                        .padding(.vertical, 10)
+                    Spacer()
+                }
+                
+            }.cornerRadius(15)
+                .border(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)), width: 1)
+                .padding()
+                .navigate(to: SignIn(), when: $sigIn)
+            
+            Text("Design by Sergey Klimovich")
+                .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                .font(.system(size: 17))
+            Text("Develop by Name Competitor")
+                .foregroundColor(Color(#colorLiteral(red: 0.3905416727, green: 0.6189041138, blue: 0.8423945904, alpha: 1)))
+                .font(.system(size: 17))
+            
+            Spacer()
+        }
     }
 }
 
